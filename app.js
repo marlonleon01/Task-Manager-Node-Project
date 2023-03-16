5fbfe133350b5dd92880cc304d05f25edcff667a
Marlon Leon <ghostveene@gmail.com>
2023-03-16 02:40:24 -0400

    using errorhandlermiddleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 5000
 const tasks = require("./routes/tasks")
 const connectDB = require("./db/connect")
 const notFound = require("./middleware/not-found")
+const errorHandlerMiddleware = require("./middleware/error-handler")
 require("dotenv").config()
 
 //middleware
@@ -13,6 +14,7 @@ app.use(express.static("./public"))
 //routes
 app.use("/api/v1/tasks", tasks)
 app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 const start = async() => {
     try {
