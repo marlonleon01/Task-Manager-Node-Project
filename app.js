c4a99b571141999c5d06e623f7fd4ed003c8909b
Marlon Leon <ghostveene@gmail.com>
2023-03-15 23:24:20 -0400

    all api/tasks routes working and tested

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+const express = require("express")
+const app = express()
+const port = 5000
+const tasks = require("./routes/tasks")
+
+app.use(express.json())
+
+app.get("/", (req,res) => {
+    res.send("Task Manager App")
+})
+
+app.use("/api/v1/tasks", tasks)
+
+app.listen(port, console.log(`Server is listening on port ${port}`))
