bac0cb2ca1c7e5b16e09180bc22e3dc43afd93bf
Marlon Leon <ghostveene@gmail.com>
2023-03-16 02:42:57 -0400

    gave ability to set port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const tasks = require("./routes/tasks")
 const connectDB = require("./db/connect")
 const notFound = require("./middleware/not-found")
