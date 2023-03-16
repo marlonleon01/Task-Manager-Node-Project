a96035a9981937a5f741da8ff121f3fa6f4775e9
Marlon Leon <ghostveene@gmail.com>
2023-03-16 00:08:17 -0400

    passed in url from .env to mongodb function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 5000
 const tasks = require("./routes/tasks")
 const connectDB = require("./db/connect")
+require("dotenv").config()
 
 //middleware
 app.use(express.json())
@@ -16,7 +17,7 @@ app.use("/api/v1/tasks", tasks)
 
 const start = async() => {
     try {
-        await connectDB()
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening on port ${port}`))
     } catch (error) {
         console.log(error)
