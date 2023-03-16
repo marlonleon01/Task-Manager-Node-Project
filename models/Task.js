2d06757b0e58cae1cf51aed03011303f2d46a1fe
Marlon Leon <ghostveene@gmail.com>
2023-03-16 00:24:17 -0400

    created new validator for completed

--- models/Task.js
+++ models/Task.js
@@ -7,7 +7,10 @@ const TaskSchema = new mongoose.Schema({
         trim: true,
         maxlength: [20, "name cannot be more than 20 characters"]
     },
-    complete: Boolean
+    complete: {
+        type: Boolean,
+        default: false, 
+    }
 })
 
 module.exports = mongoose.model("Task", TaskSchema)
